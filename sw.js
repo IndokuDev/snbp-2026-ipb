240c93433fa753780b7885ad7f00fb78df0428c5
IndokuDev <indokudev@gmail.com>
2026-04-06 19:08:46 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,18 @@
-self.addEventListener('push', (event) => {
-    const data = event.data.json();
-    self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968923.png'
-    });
+self.addEventListener('install', (e) => {
+    console.log('SW Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+    // Biar bisa dibuka offline (Basic)
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
+
+self.addEventListener('push', (e) => {
+    const data = e.data ? e.data.text() : 'Ada info baru nih!';
+    e.waitUntil(
+        self.registration.showNotification('SNPMB Update', {
+            body: data,
+            icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968923.png'
+        })
+    );
 });
